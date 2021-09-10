edc3d2f30d8a42c2f6c6f9625ef8eeb6344ec2f0
Pedro Lucas <pedrolucas.7@hotmail.com>
2021-09-10 20:58:53 -0300

    Setup inicial da aplicação

---
+++ src/main/java/br/senac/apiproduto/ECommerceApiProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.senac.apiproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/apiproduto/ECommerceApiProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.apiproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
