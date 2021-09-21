fd036a3ad4662a998cd4334115d6e7c1fe7c4cce
Pedro Lucas <pedrolucas.7@hotmail.com>
2021-09-21 19:36:44 -0300

    - Tratado exception

--- src/main/java/br/senac/apiproduto/exceptions/RestExceptionHandler.java
+++ src/main/java/br/senac/apiproduto/exceptions/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.senac.apiproduto.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +13,7 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+    // passa uma exceção por parametro, podendo ser uma excecao criada por nos ou uma exceção do java
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
@@ -23,4 +26,20 @@ public class RestExceptionHandler {
                 .path(request.getContextPath() + request.getServletPath())
                 .build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorPayload handleValidateException(MethodArgumentNotValidException exception,
+                                                HttpServletRequest request){
+        BindingResult result = exception.getBindingResult();
+        return ErrorPayload.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .error(HttpStatus.UNPROCESSABLE_ENTITY.getReasonPhrase())
+                .message(result.getFieldErrors().get(0).getDefaultMessage())
+                .path(request.getContextPath() + request.getServletPath())
+                .build();
+    }
+
 }
