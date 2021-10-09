e4ba6694970d704716a88cd5be741953ab87e0e2
Pedro Lucas <pedrolucas.7@hotmail.com>
2021-10-08 21:18:43 -0300

    - Adicionado filtro com Predicate - Paginação de Produto

--- src/main/java/br/senac/apiproduto/produto/ProdutoController.java
+++ src/main/java/br/senac/apiproduto/produto/ProdutoController.java
@@ -2,14 +2,21 @@ package br.senac.apiproduto.produto;
 
 import br.senac.apiproduto.categoria.Categoria;
 import br.senac.apiproduto.categoria.CategoriaService;
+import br.senac.apiproduto.utilitarios.Paginacao;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/produto")
@@ -18,6 +25,7 @@ public class ProdutoController {
     // PUT, GET 1 REGISTRO, GET TODOS, DELETE
     private final ProdutoService produtoService;
     private final CategoriaService categoriaService;
+    private final ProdutoRepository produtoRepository;
 
     @PostMapping("/criar")
     public ResponseEntity<ProdutoRepresentation.DetalhesProduto> cadastrarProduto(
@@ -31,13 +39,35 @@ public class ProdutoController {
     }
     // traz todos produtos
    @GetMapping("/todos")
-    public ResponseEntity<List<ProdutoRepresentation.ListaDeProduto>> buscarTodosProdutos(){
-        // fltro para buscar todos produtos que estejam com status ATIVO
-       BooleanExpression filtro = QProduto.produto.status.eq(Produto.Status.ATIVO);
+    public ResponseEntity<Paginacao> buscarTodosProdutos(
+            // Filtro com predicate utilizado para filtrar com qualquer atributo
+           @QuerydslPredicate(root = Produto.class) Predicate filtroURI,
+           @RequestParam(name = "tamanhoPagina", defaultValue = "10") int tamanhoPagina,
+           @RequestParam(name = "paginaDesejada", defaultValue = "0")int pagina){
 
-       return ResponseEntity.ok(ProdutoRepresentation.ListaDeProduto.from(
-               // chama o metodo buscarTodosProdutos do service passando o FILTRO como parametro
-               this.produtoService.buscarTodosProdutos(filtro)));
+        // Criando filtro, validando se tem algum filtro informado na URI
+       // Se for nulo filtra apenas por ativo se nao usa o ativo e o filtro da URI
+       BooleanExpression filtro = Objects.isNull(filtroURI) ?
+               QProduto.produto.status.eq(Produto.Status.ATIVO) :
+               QProduto.produto.status.eq(Produto.Status.ATIVO).and(filtroURI);
+
+       // paremetros para montar paginacao 
+       Pageable paginaDesejada = PageRequest.of(pagina, tamanhoPagina);
+
+       // passando para o repository, procurar pelo filtro e paginacao
+       Page<Produto> listaProduto = this.produtoRepository.findAll(filtro, paginaDesejada);
+
+       // monta estrutura de retorno da paginação no JSON
+       Paginacao paginacao = Paginacao.builder()
+               .paginaSelecionada(pagina)
+               .tamanhoPagina(tamanhoPagina)
+               .totalRegistros(listaProduto.getTotalElements())
+               .proximaPagina(listaProduto.hasNext())
+               .conteudo(ProdutoRepresentation.ListaDeProduto
+                       .from(listaProduto.getContent()))
+               .build();
+
+       return ResponseEntity.ok(paginacao);
     }
     // traz um produto buscando pelo id que vem na URI
     @GetMapping("/um-produto/{id}")
--- src/main/java/br/senac/apiproduto/produto/ProdutoRepository.java
+++ src/main/java/br/senac/apiproduto/produto/ProdutoRepository.java
@@ -3,10 +3,11 @@ package br.senac.apiproduto.produto;
 import com.querydsl.core.types.Predicate;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Long>,
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Long>,
         QuerydslPredicateExecutor<Produto> {
     List<Produto> findAll(Predicate filtro);
 }
